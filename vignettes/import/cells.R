fb82c6c27c80d34985295ede1e0f9f525ae6c35a
Steve Vissault <s.vissault@yahoo.fr>
2018-04-28 17:04:11 -0400

    Personal import procedure - cells

---
+++ vignettes/import/cells.R
@@ -0,0 +1,40 @@
+
+###########################
+##### Injection Cellules ##
+###########################
+
+# Authentification
+bearer <- readRDS(".httr-oauth")
+
+# Ajouter les polygones de cellules
+shp_cells <- rgdal::readOGR(dsn="./extdata/shp/",layer="cellule_terrain_BdQc")
+
+# Reprojection vers Quebec Lambert Conic
+shp_cells <- sp::spTransform(shp_cells,sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs;"))
+
+# Loop par dessus les noms de cellules pour aller chercher son polygon.
+cells_ls <- list()
+
+library(rgdal)
+library(geojsonio)
+
+for(i in 1:nlevels(shp_cells$IJ)){
+
+  cell_code <- levels(shp_cells$IJ)[i]
+  shp <- shp_cells[shp_cells$IJ == cell_code ,]
+
+  cells_ls[[i]] <- list()
+  cells_ls[[i]]$cell_code <- cell_code
+  cells_ls[[i]]$name <- shp_cells[shp_cells$IJ == cell_code ,]@data$Nom
+
+  shp_sp <- as(shp, "SpatialPolygons")
+  cells_ls[[i]]$geom <- geojsonio::geojson_list(shp)$features[[1]]$geometry
+
+  # Add CRS fields
+  cells_ls[[i]]$geom$crs <- list(type="name",properties=list(name="EPSG:4326"))
+
+}
+
+# Envoyer seulement les donnees avec un nom de cellule
+devtools::load_all(".")
+resp_cells <- post_cells(cells_ls)
