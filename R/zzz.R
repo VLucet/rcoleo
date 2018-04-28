cc6e5dcab955dda1ce045df43b5fc1c7c55bd5d1
Steve Vissault <s.vissault@yahoo.fr>
2018-04-28 17:03:26 -0400

    DEBUG mod on zzz.R with RPostgreSQL

--- R/zzz.R
+++ R/zzz.R
@@ -7,11 +7,12 @@ assign("server", "http://localhost:3001", envir = rce)
 con <- RPostgreSQL::dbConnect("PostgreSQL",user="postgres",host="localhost",dbname="coleo_dev")
 bearer <- RPostgreSQL::dbGetQuery(con,"SELECT token FROM api_keys LIMIT 1")
 saveRDS(bearer,file=".httr-oauth")
+RPostgreSQL::dbDisconnect(con)
 
 # Config de base
 assign("base", "/api/v1", envir = rce)
 assign("bearer", readRDS(".httr-oauth"), envir = rce)
-assign("ua", user_agent("rcoleo"), envir = rce )
+assign("ua", httr::user_agent("rcoleo"), envir = rce )
 
 # Point d'entrées pour le retrait ou l'analyse des données
 endpoints <- list()
