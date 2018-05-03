1ec801437682b6192644cf43e0123715ae7d90ec
Steve Vissault <s.vissault@yahoo.fr>
2018-05-03 19:11:12 -0400

    Add filters option on get_campaigns

--- R/GET_campaigns.R
+++ R/GET_campaigns.R
@@ -1,35 +1,42 @@
 #' Obtenir l'ensemble des campagnes attachées à un site
 #'
-#' @param ids `vector` contenant la liste des codes de sites pour lesquelles ont désire récupérer les campagnes
-#' @param simplify `logical` l'objet de sortie de la fonction est un `data.frame` à la place d'un objet de type `list`
-#' @inherit get_ids
+#' @param site_ids `vector` contenant les codes de sites pour lesquelles ont désire récupérer les campagnes (ex. 134_111_F01)
+#' @param dates `vector` contenant les dates de début de campagnes (ex. 2017-01-30)
+#' @param type `character` contenant le type de campagnes d'inventaires réalisé (ex. végétation)
 #' @examples
 #' get_campaigns()
 #' @export
 
-get_campaigns <- function(ids=NULL, ...){
+get_campaigns <- function(site_ids = NULL, dates = NULL, type = NULL, ...){
 
-  ##### Filter by type and date campagne?
-  ### SOL1: rebuild query, avoid using get_ids(), use get_gen()
-  ### Ajout de fonctionnalitees, je veux aller chercher tous les types de campagne vegetation pour injecter en batch l'effort qui est le meme dans ce cas.
+  endpoint <- rce$endpoints$campaigns
 
-  if(!is.null(ids)){
-    sites <- get_sites(ids, rce$endpoints$sites)
+  if(all(length(site_ids) == length(dates) && !is.null(site_ids) && !is.null(type))){
+    stop("Les deux vecteurs `ids`, `dates` doivent avoir une taille identique")
+  }
 
-    responses <- list()
+  if(all(length(type) > 1 && !is.null(type))){
+    stop("un seul type peut être spécifié à la fois")
+  }
 
-    ## Est-ce que c'est une bonne idee que je perde la structure list(body=body, response=response)?
-    ## Non, write utils called simplify which turn into data.frame the responses
+  if(!is.null(site_ids)){
+    sites <- get_sites(ids=site_ids, rce$endpoints$sites)
+    fkey_ids <- sapply(sites,function(x) x$body[,"id"])
+  }
 
-    for(id in 1:length(sites)){
-      responses[[id]] <- list(site_code = NULL, campaigns = NULL)
-      responses[[id]]$site_code <- sites[[id]]$body$site_code
-      responses[[id]]$campaigns <- do.call("rbind",sites[[id]]$body$campaigns)
-    }
+  # tests args to set iterator
+  len_args <- c(length(site_ids),length(dates))
+  len <- unique(len_args[which(len_args>0)])
 
-    return(responses)
+  responses <- list()
+
+  # Prep query
+  for(r in 1:len){
+    query <- list(site_id=fkey_ids[r], date = dates[r],type = type)
+    responses[[r]] <- get_gen(endpoint,query=query)
   }
 
 
+  return(responses)
 
 }
