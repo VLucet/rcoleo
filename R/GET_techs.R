de5b9aa772dac133c1ac5456302b2f804d898693
Steve Vissault <s.vissault@yahoo.fr>
2018-06-20 12:43:04 -0400

    Add get_techs()

---
+++ R/GET_techs.R
@@ -0,0 +1,38 @@
+#' Obtenir l'ensemble des techiciens attachés à une campagne
+#'
+#' @param name `vector` contenant le prénom du technicien (sensible à la case)
+#' @param lastname `vector` contenant le nom du technicien (sensible à la case)
+#' @examples
+#' get_techs(name="Caroline")
+#' @export
+
+get_techs <- function(name = NULL, lastname = NULL, ...){
+
+  responses <- list()
+  endpoint <- rce$endpoints$technicians
+
+  if (all(is.null(name),is.null(lastname))) {
+
+    # Obtenir tous les sites
+    responses <- get_gen(endpoint)
+
+  } else {
+
+    # tests args to set iterator
+    len_args <- c(length(name),length(lastname))
+    len <- unique(len_args[which(len_args>0)])
+    stopifnot(length(len)==1)
+
+    # Prep query
+    for(r in 1:len){
+
+      query <- list(lastname = lastname[r],
+                    name = name[r])
+
+      responses[[r]] <- get_gen(endpoint,query=query)
+    }
+  }
+
+  return(responses)
+
+}
