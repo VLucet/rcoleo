7f2971c9b84716d4755525da890f4d8d1d3211ad
Steve Vissault <s.vissault@yahoo.fr>
2018-06-20 12:43:13 -0400

    Cover function without aguments

--- R/GET_sites.R
+++ R/GET_sites.R
@@ -1,14 +1,47 @@
 #' Obtenir les informations sur les sites depuis l'API de coleo
-#'
-#' @inherit get_ids
+#' @param site_code est un vecteur contenant les identifiants uniques que l'on désire obtenir. Si site_code n'est pas spécifié, la fonction retournera l'ensemble des entrées présentes dans la table cells.
 #' @examples
-#' get_sites(ids=c('135_104_H01'))
+#' get_sites(site_code=c('135_104_H01'))
 #' get_sites()
 #' @export
 
 
-get_sites <- function(ids = NULL, ...) {
+get_sites <- function(site_code = NULL, ...) {
 
-  return(get_ids(ids, rce$endpoints$sites, ...))
+  responses <- list()
+  endpoint <- rce$endpoints$sites
+
+  if (is.null(site_code)) {
+
+    # Obtenir tous les sites
+    responses <- get_gen(endpoint)
+
+  } else {
+
+    stopifnot(is.character(site_code))
+
+    # Obtenir les sites définis dans ids
+    for (id in 1:length(site_code)) {
+
+      # prep query
+      responses[[id]] <- unlist(get_gen(endpoint, query = list(site_code = site_code[id])), recursive=FALSE)
+
+      if (length(responses[[id]]$body) == 0) {
+
+        message(site_code[id], " n'est pas présent dans la base de données")
+
+      } else if (nrow(responses[[id]]$body) > 1) {
+
+        message(nrow(responses[[id]]$body), " entrées pour le numéro du site: ",
+          site_code[id])
+
+      }
+
+    }
+  }
+
+
+  return(responses)
+
 
 }
