5a96c4c6009cb92eb996fb314ac0ae3504aa483a
Steve Vissault <s.vissault@yahoo.fr>
2018-07-26 11:13:48 -0400

    Adapt function to new species schema (rank field)

--- R/GET_species.R
+++ R/GET_species.R
@@ -1,7 +1,7 @@
 #' Retourne les entrées d'espèces présentes dans la base de données
 #'
-#' @param species `character` vecteur contenant les espèces recherché (sensible à la case) - Obsolète si l'argument query est spécifié
-#' @param genus `character` vecteur contenant les genres d'espèce recherché (sensible à la case) - Obsolète si l'argument query est spécifié
+#' @param name `character` vecteur contenant les noms taxonomiques recherché (sensible à la case) - Obsolète si l'argument query est spécifié
+#' @param rank `character` vecteur contenant les rangs taxonomiques recherché (sensible à la case) - Obsolète si l'argument query est spécifié
 #' @param vernacular_fr `character` vecteur contenant les noms vernaculaires en francais des espèces recherchées (sensible à la case) - Obsolète si l'argument query est spécifié
 #' @inheritParams get_gen
 #' @return
@@ -11,13 +11,13 @@
 #' get_species()
 #' @export
 
-get_species <- function(species = NULL, genus = NULL, vernacular_fr = NULL,...) {
+get_species <- function(name = NULL, rank = NULL, vernacular_fr = NULL,...) {
 
   responses <- list()
   query <- list()
-  endpoint <- rce$endpoints$species
+  endpoint <- rce$endpoints$taxa
 
-  if (all(is.null(species),is.null(genus),is.null(vernacular_fr))) {
+  if (all(is.null(name),is.null(rank),is.null(vernacular_fr))) {
 
     # Obtenir toutes les cellules
     responses <- get_gen(endpoint, ...)
@@ -25,12 +25,12 @@ get_species <- function(species = NULL, genus = NULL, vernacular_fr = NULL,...) 
   } else {
 
     # tests args to set iterator
-    len <- max(c(length(species),length(genus),length(vernacular_fr)))
+    len <- max(c(length(name),length(rank),length(vernacular_fr)))
 
     # Obtenir des cellules specifiques (query)
     for (r in 1:len) {
-      query <- list(species= species[r],
-                    genus = genus[r],
+      query <- list(name = name[r],
+                    rank = rank[r],
                     vernacular_fr = vernacular_fr[r])
 
       # Make sure NULL is passed to the query instead of NA
