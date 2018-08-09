33fe47da46a4151d8bf8376b504b34098f2c7ba8
Steve Vissault <s.vissault@yahoo.fr>
2018-08-09 12:22:39 -0400

    Ajout des fonctions spatiales

---
+++ R/sf_utils.R
@@ -0,0 +1,62 @@
+#' Fonction générique pour convertir les sorties de l'API en objet de classe spatiale (sf)
+#'
+#' @param responses `list` objet retourné par les fonctions GET du package coléo (ex. get_sites()), voir @details
+#' @return
+#' Retourne un objet de type `list` contenant les réponses de l'API. Chaque niveau de la liste correspond à une page. Pour chacun des appels sur l'API (page), la classe retourné est `getSuccess` ou `getError`. Une réponse de classe `getSuccess` est une liste à deux niveaux composé du contenu (`body`), et la réponse [httr::response]. Une réponse de classe `getError` dispose de la même structure mais ne contiendra pas de body, seulement la réponse de l'API.
+#' @examples
+#' resp <- get_sites()
+#' plot(cl_to_sf(resp))
+#' @details L'objet d'entrée doit contenir une colonne `geom.type` & `geom.coordinates`
+#' @seelso [cells_sf()], [sites_cf()]
+#' @export
+
+cl_to_sf <- function(responses = responses) {
+
+  # Check for getSuccess classes
+  all(lapply(responses,class) == "getSuccess")
+
+  # get features
+  features <- unlist(lapply(responses, function(page){
+    feature <- apply(page$body, 1, function(feature){
+      return(list(type="Feature", geometry=list(type=feature$geom.type,coordinates=feature$geom.coordinates)))
+    })
+    return(feature)
+  }), recursive=FALSE)
+
+  names(features) <- NULL
+
+  # get Data
+  geom_s <- sf::read_sf(
+    jsonlite::toJSON(
+      list(
+        type="FeatureCollection",
+        features=features),
+        auto_unbox=TRUE
+    )
+  )
+
+  geom_df <- lapply(responses, function(x) dplyr::select(x$body, -geom.type, -geom.coordinates))
+  geom_df <- do.call(rbind,geom_df)
+
+  # Data binding
+  geom_sdf <- sf::st_sf(dplyr::bind_cols(geom_df,geom_s))
+
+  return(geom_sdf)
+}
+
+#' Fonction retournant les cellules dans un objet de classe spatiale (sf)
+#'
+#' @param ... arguments de la fonction `httr::GET()` et `rcoleo::get_cells()`
+#' @seealso [get_cells()]
+
+cells_sf <- function(...){
+  return(cl_to_sf(get_cells(...)))
+}
+
+#' Fonction retournant les celsites dans un objet de classe spatiale (sf)
+#'
+#' @param ... arguments de la fonction `httr::GET()` et `rcoleo::get_sites()`
+#' @seealso [get_sites()]
+sites_sf <- function(...){
+  return(cl_to_sf(get_sites(...)))
+}
