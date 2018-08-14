ae40c65a29f677e2040c59d56c37d6ed179d5b91
Steve Vissault <s.vissault@yahoo.fr>
2018-08-14 12:12:31 -0400

    add method as.data.frame()

--- R/api_utils.R
+++ R/api_utils.R
@@ -6,3 +6,32 @@
 diagnostic <- function(responses=responses){
   do.call(plyr::rbind.fill,lapply(responses,function(x) as.data.frame(httr::http_status(x$response))))
 }
+
+#' Transformer la liste de réponses en data.frame
+#'
+#' @param responses liste de réponse des fonctions de GET/POST
+#' @export
+
+as.data.frame.coleoGetResp <- function(responses=responses){
+
+  stopifnot(is.coleoGet(responses))
+
+  # Loop over query
+  all_body <- lapply(responses, function(query){
+    query <- responses[[1]]
+    # if query contains multipages
+    if(length(query) > 1){
+      return(do.call(plyr::rbind.fill,lapply(query, function(page) return(page$body))))
+    } else {
+      return(query[[1]]$body)
+    }
+  })
+
+  return(as.data.frame(do.call(plyr::rbind.fill, all_body)))
+
+}
+
+#' S3 Validators
+
+is.coleoGet <- function(x) inherits(x,'coleoGetResp')
+is.coleoPost <- function(x) inherits(x,'coleoPostResp')
