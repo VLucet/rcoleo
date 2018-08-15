0b808ea39cd363baa609f20e2994e003653caa36
Steve Vissault <s.vissault@yahoo.fr>
2018-08-15 14:39:00 -0400

    Cover case empty body in as.data.frame.coleoGetResp

--- R/api_utils.R
+++ R/api_utils.R
@@ -9,17 +9,24 @@ as.data.frame.coleoGetResp <- function(responses=responses){
 
   # Loop over query
   all_body <- lapply(responses, function(query){
-    query <- responses[[1]]
     # if query contains multipages
     if(length(query) > 1){
-      return(do.call(plyr::rbind.fill,lapply(query, function(page) return(page$body))))
+      return(do.call(
+        plyr::rbind.fill,
+        lapply(query, function(page){
+         return(page$body)
+        })
+      ))
     } else {
-      return(query[[1]]$body)
+      if(length(query[[1]]$body) == 0){
+        return(NULL)
+      } else {
+        return(query[[1]]$body)
+      }
     }
   })
 
   return(as.data.frame(do.call(plyr::rbind.fill, all_body)))
-
 }
 
 #' Test sur la classe `coleoGetResp` (Objet S3)
