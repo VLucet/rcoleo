300e1553bf2e52c771787b948a48f2162d60f620
Steve Vissault <s.vissault@yahoo.fr>
2018-10-31 10:10:13 -0400

    Add new POST functions

---
+++ R/POST_lures.R
@@ -0,0 +1,29 @@
+#' Publication d'un piège dans l'API de coléo
+#'
+#' Cette fonction applique la méthode POST sur le point d'entrées `lures` de l'API de Coleo
+#'
+#' @inheritParams post_cells
+#' @export
+
+post_lures <- function(data, ...) {
+
+  # Preparation de l'objet de sortie
+  responses <- list()
+  class(responses) <- "coleoPostResp"
+
+  endpoint <- endpoints()$lures
+
+  for (i in 1:length(data)) {
+    
+    # On retourne l'id unique pour la campagne à laquelle est rattaché les trappes
+    # Le unlist c'est pour les pages, mais je sais que la réponse contient une seule page (match sur un code)
+    campaign_id <- as.data.frame(get_campaigns(site_code=data[[i]]$site_code,opened_at=data[[i]]$opened_at,closed_at=data[[i]]$closed_at,type=data[[i]]$type))$id
+    stopifnot(length(campaign_id) == 1)
+
+    campaign_id <- data[[i]]$campaign_id
+
+    responses[[i]] <- post_gen(endpoint, data[[i]], ...)
+  }
+
+  return(responses)
+}
