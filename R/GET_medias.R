480840e6647868882ee1edc5403c87823b608fda
Steve Vissault <s.vissault@yahoo.fr>
2019-06-12 13:14:43 -0400

    Simplify medias

--- R/GET_medias.R
+++ R/GET_medias.R
@@ -1,53 +1,41 @@
 #' Téléverse l'ensemble des médias appartenant à une campagnes
 #'
-#' @param site_code `character` vecteur contenant les codes de sites pour lesquelles ont désire récupérer les campagnes (ex. 134_111_F01)
-#' @param opened_at `character` vecteur contenant les dates de début de campagnes (ex. 2017-01-30)
-#' @param closed_at `character` vecteur contenant les dates de fin de campagnes (ex. 2017-01-30)
-#' @param type `character` vecteur contenant le type de campagnes d'inventaires réalisé (ex. végétation)
+#' @param x `vector` of campaign ids
 #' @inheritParams get_gen
 #' @return
 #' Sauvegarde les images/sons attachés aux campagnes à une emplacement défini par l'utilisateur
 #' @examples
 #' \dontrun{
-#' list_medias(site_code="141_108_F01", type="mammifères")
+#' list_medias(x = c(101,104))
 #' Par exemple, on veut obtenir toutes les photos prises dans le cadre d'une campagne sur le site 141_108_F01 et portant sur les mammifères 
-#' get_medias(site_code="141_108_F01", type="mammifères")
+#' get_medias(list_medias(x = c(101,104)))
 #' }
 #' @export
 
 
-list_medias <- function(site_code = NULL, opened_at = NULL, closed_at = NULL, type = NULL, ...){
+list_medias <- function(x, ...){
 # Preparation de l'objet de sortie
   responses <- list()
   class(responses) <- "coleoGetResp"
 
   endpoint <- endpoints()$media
 
-  # Si tous les arguments sont nuls
-  if(all(is.null(site_code), is.null(opened_at), is.null(closed_at), is.null(type))){
+  for(r in 1:length(x)){
+    responses[[r]] <- get_gen(endpoint, query = list(campaign_id = x[r]), ...)
+  } 
 
-    stop("Au moins un des arguments `site_code`, `opened_at`, `closed_at` et `type` doit être spécifié")
+  media <- as.data.frame(responses)
 
-  } else {
-
-    campaigns <- as.data.frame(get_campaigns(site_code, opened_at, closed_at, type))
-
-    for(r in 1:nrow(campaigns)){
-      responses[[r]] <- get_gen(endpoint, query = list(campaign_id = campaigns$id[r]), ...)
-    } 
-
-    media <- as.data.frame(responses)
-
-    # On regarde si la campagne contient des médias
-    if(nrow(media) == 0){
-      stop("Aucun média attaché à ces campagnes")
-    }
-
-    media$uri <- paste0("/media/",media$type,"/",media$uuid,"/original")
-
-    return(media)
-
+  # On regarde si la campagne contient des médias
+  if(nrow(media) == 0){
+    stop("Aucun média attaché à ces campagnes")
   }
+
+  media$uri <- paste0("/media/",media$type,"/",media$uuid,"/original")
+
+  class(media) <- c("tbl_df", "tbl", "data.frame", "listMedias")
+
+  return(media)
 }
 
 #' Télécharge l'ensemble des médias appartenant à une campagnes
@@ -64,20 +52,21 @@ list_medias <- function(site_code = NULL, opened_at = NULL, closed_at = NULL, ty
 #' }
 #' @export
 
-get_medias <- function(site_code = NULL, opened_at = NULL, closed_at = NULL, type = NULL, output_dir = "./media", verbose = TRUE, ...) {
+get_medias <- function(media, output_dir = "./media", verbose = TRUE, ...) {
 
-  media <- list_medias(site_code, opened_at, closed_at, type)
+  stopifnot("listMedias" %in% class(media))
 
   # On créer le répertoire de sortie, s'il n'existe pas
   dir.create(output_dir)
-
+  cat("Téléversement en cours... \n")
+  cat("0 sur", nrow(media), "\r")
   # Mieux gérer la progress bar
   # Vérifier si bug dans les chemins avec Windows
   for(m in 1:nrow(media)){
     if(verbose){
-      cat("Téléversement de", m ,"/",nrow(media), "\n")
-      cat("Fichier:", media$name[m], "\n")
-      cat("Type:", media$type[m], "\n")
+      if((m %% 100) == 0) {
+        cat(m, "sur", nrow(media), "\r")
+      }
     }
     httr::GET(
     url = paste0(server(),media$uri[m]), 
