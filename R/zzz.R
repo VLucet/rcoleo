1ef6ee46d7cf320eede4a35aaa454f480c5310df
Guillaume Larocque <glaroc@gmail.com>
2020-04-22 12:19:30 -0400

    Experiment to fix prod

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 # Config de base
 #server <- function() "https://coleo.biodiversite-quebec.ca"
-server <- function() "http://localhost:8080" # dev purpose
+server <- function() "http://localhost:3838" # dev purpose
 base <- function() "/api/v1"
 bearer <- function() ifelse(file.exists(".httr-oauth"), as.character(readRDS(".httr-oauth")), NA)
 ua <- httr::user_agent("rcoleo")
